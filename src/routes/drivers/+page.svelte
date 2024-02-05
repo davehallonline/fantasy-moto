c5a02179154f9d9d8a867eea64875f51d7c9a8e4
David Hall <davehall98@gmail.com>
2024-02-05 19:42:50 +0000

    Initial working project with db and auth

---
+++ src/routes/drivers/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+    <h3>My List of Drivers...</h3>
+	<ul>
+		{#each data.drivers as driver}
+		  <li>{driver.driver_name}</li>
+		{/each}
+	</ul>
+</section>
