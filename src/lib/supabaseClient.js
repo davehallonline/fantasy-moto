023148e156b3d4905c8f88d98bc31b522429de07
David Hall <davehall98@gmail.com>
2024-02-11 18:03:16 +0000

    Changes required for building to Vercel

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,15 @@
 
 import { createClient } from '@supabase/supabase-js'
+import { building } from '$app/environment'
 import { env } from '$env/dynamic/public'
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+const fallbackURL = 'http://localhost:3067'
+export const PUBLIC_SUPABASE_URL = building ? fallbackURL : env.PUBLIC_SUPABASE_URL || 'http://localhost:3067'
+
+const fallbackKey = 'abcdef'
+export const PUBLIC_SUPABASE_ANON_KEY = building ? fallbackKey : env.PUBLIC_SUPABASE_ANON_KEY || 'abcdef'
+
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+
+
+//export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
