8b589ad49a108b301b11e14e29becfb9fe0bbef7
David Hall <davehall98@gmail.com>
2024-03-02 14:54:18 +0000

    Minor login changes

--- src/routes/home/+page.server.js
+++ src/routes/home/+page.server.js
@@ -1,6 +1,14 @@
 import { supabase } from "$lib/supabaseClient";
+import { redirect } from '@sveltejs/kit'
 
-export async function load() {
+export async function load({ params, locals:{supabase, getSession} }) {	
+
+  const session = await getSession()
+
+  if (!session) {
+    throw redirect(303, '/')
+  }
+
   const { data } = await supabase.from('Events').select('*');
   return {
     events: data ?? [],
